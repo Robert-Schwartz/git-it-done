442be2c87f51028175a0e4c0859e76f3abb39c12
Robert Schwartz <bschwartz23@gmail.com>
2021-04-19 22:08:28 -0700

    add catch to fetch

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -10,10 +10,20 @@ var getUserRepos = function (username) {
   var apiUrl = "https://api.github.com/users/" + username + "/repos";
 
   // make a request to the API url
-  fetch(apiUrl).then(function (response) {
-    response.json().then(function (data) {
-      displayRepos(data, username);
-    });
+	fetch(apiUrl)
+  .then(function(response) {
+    // request was successful
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data, user);
+      });
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  })
+  .catch(function(error) {
+    // Notice this `.catch()` getting chained onto the end of the `.then()` method
+    alert("Unable to connect to GitHub");
   });
 };
 
@@ -36,6 +46,11 @@ userFormEl.addEventListener("submit", formSubmitHandler);
 
 //display Repos on page
 var displayRepos = function (repos, searchTerm) {
+	// check if api returned any repos
+if (repos.length === 0) {
+  repoContainerEl.textContent = "No repositories found.";
+  return;
+}
   // clear old content
   repoContainerEl.textContent = "";
   repoSearchTerm.textContent = searchTerm;
